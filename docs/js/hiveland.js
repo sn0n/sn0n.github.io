24acc417b7d13c8e110d0cece2753286872986e9
sn0n <are.zee.dee@gmail.com>
2022-10-29 01:34:21 -0700

    canvas things

--- docs/js/hiveland.js
+++ docs/js/hiveland.js
@@ -1,3 +1,25 @@
-function DisplayPostFAQ() {
-    modal.style.display = "block";
-}
+window.onload = (event) => {
+    draw();
+  };
+
+
+
+  var canvas = document.getElementById('blokzCanva');
+  var heightRatio = 0.5;
+
+  canvas.height = canvas.width * heightRatio;
+          function draw() {
+          if (canvas.getContext) {
+            const ctx = canvas.getContext("2d");
+  
+            ctx.fillStyle = "rgb(200, 0, 0)";
+            ctx.fillRect(10, 10, 50, 50);
+  
+            ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+            ctx.fillRect(30, 30, 50, 50);
+          }
+        }
+
+
+
+        
--- src/pages/index.md
+++ src/pages/index.md
@@ -5,24 +5,7 @@
   
 </center>
 
-<script>
-window.onload = (event) => {
-  draw();
-};
-
-        function draw() {
-        const canvas = document.getElementById("blokzCanva");
-        if (canvas.getContext) {
-          const ctx = canvas.getContext("2d");
-
-          ctx.fillStyle = "rgb(200, 0, 0)";
-          ctx.fillRect(10, 10, 50, 50);
-
-          ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-          ctx.fillRect(30, 30, 50, 50);
-        }
-      }
-</script>  
+<script defer src="../js/hiveland.js"></script>
 
 to sn0n.com. The personal website of Rob Foraker, erm,... sn0n.
 
